cacfa33bbf6a7fc44c945f58ac94498971e494a1
youaregoo <1508496092@qq.com>
2018-08-08 10:59:50 +0800

    错误提示翻译成英文

--- library/src/main/java/ezy/boost/update/UpdateError.java
+++ library/src/main/java/ezy/boost/update/UpdateError.java
@@ -78,25 +78,25 @@ public class UpdateError extends Throwable {
     public static final SparseArray<String> messages = new SparseArray<>();
     static {
 
-        messages.append(UPDATE_IGNORED, "该版本已经忽略");
-        messages.append(UPDATE_NO_NEWER, "已经是最新版了");
+        messages.append(UPDATE_IGNORED, "This version has been ignored");//该版本已经忽略
+        messages.append(UPDATE_NO_NEWER, "Already the latest version");//已经是最新版了
 
-        messages.append(CHECK_UNKNOWN, "查询更新失败：未知错误");
-        messages.append(CHECK_NO_WIFI, "查询更新失败：没有 WIFI");
-        messages.append(CHECK_NO_NETWORK, "查询更新失败：没有网络");
-        messages.append(CHECK_NETWORK_IO, "查询更新失败：网络异常");
-        messages.append(CHECK_HTTP_STATUS, "查询更新失败：错误的HTTP状态");
-        messages.append(CHECK_PARSE, "查询更新失败：解析错误");
+        messages.append(CHECK_UNKNOWN, "Query update failed: unknown error");//查询更新失败：未知错误
+        messages.append(CHECK_NO_WIFI, "Query update failed: no WIFI");//查询更新失败：没有 WIFI
+        messages.append(CHECK_NO_NETWORK, "Query update failed: no network");//查询更新失败：没有网络
+        messages.append(CHECK_NETWORK_IO, "Query update failed: network exception");//查询更新失败：网络异常
+        messages.append(CHECK_HTTP_STATUS, "Query update failed: wrong HTTP status");//查询更新失败：错误的HTTP状态
+        messages.append(CHECK_PARSE, "Query update failed: parsing error");//查询更新失败：解析错误
 
-        messages.append(DOWNLOAD_UNKNOWN, "下载失败：未知错误");
-        messages.append(DOWNLOAD_CANCELLED, "下载失败：下载被取消");
-        messages.append(DOWNLOAD_DISK_NO_SPACE, "下载失败：磁盘空间不足");
-        messages.append(DOWNLOAD_DISK_IO, "下载失败：磁盘读写错误");
-        messages.append(DOWNLOAD_NETWORK_IO, "下载失败：网络异常");
-        messages.append(DOWNLOAD_NETWORK_BLOCKED, "下载失败：网络中断");
-        messages.append(DOWNLOAD_NETWORK_TIMEOUT, "下载失败：网络超时");
-        messages.append(DOWNLOAD_HTTP_STATUS, "下载失败：错误的HTTP状态");
-        messages.append(DOWNLOAD_INCOMPLETE, "下载失败：下载不完整");
-        messages.append(DOWNLOAD_VERIFY, "下载失败：校验错误");
+        messages.append(DOWNLOAD_UNKNOWN, "Download failed: unknown error");//下载失败：未知错误
+        messages.append(DOWNLOAD_CANCELLED, "Download failed: download was canceled");//下载失败：下载被取消
+        messages.append(DOWNLOAD_DISK_NO_SPACE, "Download failed: insufficient disk space");//下载失败：磁盘空间不足
+        messages.append(DOWNLOAD_DISK_IO, "Download failed: disk read and write error");//下载失败：磁盘读写错误
+        messages.append(DOWNLOAD_NETWORK_IO, "Download failed: network exception");//下载失败：网络异常
+        messages.append(DOWNLOAD_NETWORK_BLOCKED, "Download failed: network outage");//下载失败：网络中断
+        messages.append(DOWNLOAD_NETWORK_TIMEOUT, "Download failed: network timeout");//下载失败：网络超时
+        messages.append(DOWNLOAD_HTTP_STATUS, "Download failed: wrong HTTP status");//下载失败：错误的HTTP状态
+        messages.append(DOWNLOAD_INCOMPLETE, "Download failed: download is incomplete");//下载失败：下载不完整
+        messages.append(DOWNLOAD_VERIFY, "Download failed: checksum error");//下载失败：校验错误
     }
 }
